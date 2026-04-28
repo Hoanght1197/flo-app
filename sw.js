ca400ab3345f372f7f6ad8d7d97212d7ff1c8560
Hoanght1197 <johnice1990@gmail.com>
2026-04-28 09:01:50 +0700

    Bust service worker cache v3 to force fresh reload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chu-ky-v1';
+const CACHE = 'chu-ky-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
